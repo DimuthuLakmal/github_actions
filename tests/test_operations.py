45e446f1b6fb62ab4d38f120bbf2896bf95dda17
Dimuthu Lakmal <kjtdimuthu@gmail.com>
2026-07-07 10:05:43 +1000

    Added unit test cases

---
+++ tests/test_operations.py
@@ -0,0 +1,10 @@
+from src.math_operations import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 2) == 0
+
+def test_subtract():
+    assert subtract(1, 2) == -1
+    assert subtract(-1, 2) == -3
+
