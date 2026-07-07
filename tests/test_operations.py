12a391337cc1c47e89deec3e973b70da65abb855
Dimuthu Lakmal <kjtdimuthu@gmail.com>
2026-07-07 10:17:53 +1000

    Fixed the test issue

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,7 +2,7 @@ from src.math_operations import add, subtract
 
 def test_add():
     assert add(1, 2) == 3
-    assert add(-1, 2) == 0
+    assert add(-1, 1) == 0
 
 def test_subtract():
     assert subtract(1, 2) == -1
